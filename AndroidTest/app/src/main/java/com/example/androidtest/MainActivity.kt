ff619448cb079365a6b63ee832c35eea8e2929bb
AaronCChen2233 <32030679+AaronCChen2233@users.noreply.github.com>
2020-01-22 09:47:16 -0800

    webview test

--- AndroidTest/app/src/main/java/com/example/androidtest/MainActivity.kt
+++ AndroidTest/app/src/main/java/com/example/androidtest/MainActivity.kt
@@ -1,24 +1,107 @@
 package com.example.androidtest
 
+import android.annotation.SuppressLint
+import android.content.Context
 import android.content.Intent
+import android.content.res.Configuration
+import android.os.Build
 import android.os.Bundle
+import android.os.Handler
+import android.util.AttributeSet
 import android.view.View
+import android.webkit.ValueCallback
+import android.webkit.WebView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import org.w3c.dom.Document
-import org.xml.sax.InputSource
-import java.io.File
 import java.io.InputStream
-import java.io.StringReader
+import java.io.InputStreamReader
 import javax.xml.parsers.DocumentBuilder
 import javax.xml.parsers.DocumentBuilderFactory
 
 
 class MainActivity : AppCompatActivity() {
 
+    val myHandler = Handler()
+
+    @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
+        val browser: WebView = findViewById(R.id.testwebView) as WebView
+        val settings = browser.settings
+        settings.javaScriptCanOpenWindowsAutomatically = true
+        settings.javaScriptEnabled = true
+        settings.allowFileAccess = true
+        settings.domStorageEnabled = true
+
+        browser.loadUrl("file:///android_asset/musictest.html")
+        val str ="e/4"
+        browser.post {
+            run {
+                browser.loadUrl("javascript:musictest(\"$str\")")
+            }
+        }
+
+//        browser.evaluateJavascript("javascript:musictest();",object : ValueCallback<String> {
+//                override fun onReceiveValue(value: String) {
+//
+//                }
+//
+//            })
+
+        //        var musictest = CallJavaScript.callFunction(this)
+
     }
+
+
+//    object CallJavaScript {
+//
+//        fun callFunction(mContext: android.content.Context): Any? {
+//
+//            var jsResult: Any? = null
+//
+//            val params = arrayOf<Any>("")
+//
+//            // Every Rhino VM begins with the enter()
+//            // This Context is not Android's Context
+//            val rhino = org.mozilla.javascript.Context.enter()
+//
+//            // Turn off optimization to make Rhino Android compatible
+//            rhino.optimizationLevel = -1
+//            try {
+//                val scope = rhino.initStandardObjects()
+//
+//                // Note the forth argument is 1, which means the JavaScript source has
+//                // been compressed to only one line using something like YUI
+//
+//                val assetManager = mContext.assets
+//                try {
+//                    val input = assetManager.open("music.js")
+//                    val targetReader = InputStreamReader(input)
+//                    rhino.evaluateReader(scope, targetReader, "JavaScript", 1, null)
+//                } catch (e: Exception) {
+//                    e.printStackTrace()
+//                }
+//
+//
+//                // Get the functionName defined in JavaScriptCode
+//                val obj = scope.get("musictest", scope)
+//
+//                if (obj is org.mozilla.javascript.Function) {
+//
+//                    // Call the function with params
+//                    jsResult = obj.call(rhino, scope, scope, params)
+//                    // Parse the jsResult object to a String
+//                    val result = org.mozilla.javascript.Context.toString(jsResult)
+//                }
+//            } finally {
+//                org.mozilla.javascript.Context.exit()
+//            }
+//            return jsResult
+//        }
+//    }
 
     fun openFileClick(view: View) {
         val intent = Intent()
